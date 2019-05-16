490c4e60b32d7554622eeebe4f7d98a815bab736
chaincan <cc16023786@163.com>
2019-05-16 12:26:24 +0800

    【Test007_Integer】-【char占两个字节】

--- Practice1/src/Test007_Integer.java
+++ Practice1/src/Test007_Integer.java
@@ -1,15 +1,27 @@
+package com.practice.basic;
 
 public class Test007_Integer {
 
 	public static void main(String[] args) {
-		Float floatNum = new Float(3.6f);
-		System.out.println(floatNum.intValue());
+//		Float floatNum = new Float(3.6f);
+//		System.out.println(floatNum.intValue());
 		
 		//下面两个等价，Use "java.util.Random.nextInt()" instead，推荐用后者
 		//产生100以内的随机数
 //		System.out.println((int)(Math.random()*100));
 //		System.out.println(new Random().nextInt(100));
+		System.out.println("mix int value: "+Integer.MIN_VALUE);
+		System.out.println("max int value: "+Integer.MAX_VALUE+" after +1 :"+(Integer.MAX_VALUE+1));
+		
+		//Java中没有无符号数；
+		//java中，采用补码的形式表示负数。对于负数来讲，它的反码就是除去符号位取反，然后加1就得到了它的补码。(负数取反加一)
+		int positiveNum = 1;
+		int nagetiveNum = -1;
+		System.out.println(positiveNum+" binary format: "+Integer.toBinaryString(positiveNum));
+		System.out.println(nagetiveNum+" binary format: "+Integer.toBinaryString(nagetiveNum));
 
+		char simpleChar = '粲';
+		System.out.print("一个char占 "+Character.BYTES+" 个字节,可以存储一个汉字 -》"+simpleChar);
 	}
 
 }
